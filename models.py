742d6d6f51cb645ed431cd86c444ca75f11735ec
markbogdan68-ship-it <markbogdan68@gmail.com>
2025-09-26 15:52:22 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+
 
 class Base(DeclarativeBase):
+    """База для всех моделей"""
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    tg_id: Mapped[int] = mapped_column(Integer, unique=True, index=True)
+    username: Mapped[str] = mapped_column(String, default="")
